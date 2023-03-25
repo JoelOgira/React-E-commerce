0be4f9a53ba0fdd76c52a1ef0f0f12831e9e0c6d
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-03-25 06:32:54 +0300

    Created api component

---
+++ src/api/api.jsx
@@ -0,0 +1,20 @@
+// import axios from "axios";
+
+// const api = axios.create({
+    // baseURL: ' http://localhost:6000/'
+// })
+
+// export const fetchUsers = async () => {
+//     const res = await api.get('/users');
+//     return res.data;
+// }
+
+// export const fetchUser = async (id) => {
+//     const res = await api.get(`/users/${id}`);
+//     return res.data;
+// }
+
+// export const updateUser = async({id, ...updatedUser}) => {
+//     const res = await api.put(`/users/${id}`, updatedUser);
+//     return res.data;
+// }
