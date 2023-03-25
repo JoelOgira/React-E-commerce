ab33be9aa276c9905a0a539b9c76a7abc4b3b3dc
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-03-25 06:28:40 +0300

    Added react query for fetching and state management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+      <Routes>
+        <Route path='*' element={<App/>}/>
+      </Routes>
+      <ReactQueryDevtools position='bottom-left' initialIsOpen='true' />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
