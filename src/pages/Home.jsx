5fa0088d1d267df34aa9c872b2494b7b943aca0a
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-05-10 13:09:34 +0300

    Added functionality to the component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,30 @@
-import { useQuery } from "@tanstack/react-query";
-import * as api from '../api/api';
+import { useContext } from 'react';
+import ProductContext from '../contexts/ProductContext';
+import Items from '../Components/Items';
 
-const Home = () => {
+const Product = () => {
 
-    const {data, isLoading, isError, error} = useQuery({
-        queryKey:['users'],
-        queryFn: api.fetchUsers
-    })
+    const { searchResults:products, isLoading, error } = useContext(ProductContext);
 
-    console.log(data);
+    const filteredProducts = products.filter((product) => (product?.category === "men's clothing") || (product?.category === "women's clothing"))
 
     return (
-        <div className="px-12 md:container md:mx-auto md:px-0">
-            {JSON.stringify(data)}
+        <div>
+            {isLoading && <p className='container mx-auto'>Your Products Loading... </p>}
+            {!isLoading && error && <p className='container mx-auto'>There was an Error: {error.message}</p>}
+            {!isLoading && !error && 
+                <section className='py-16'>
+                    <div className='container mx-auto'>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
+                            {(filteredProducts || [])?.map((product) => 
+                               <Items product={product} key={product?.id} /> 
+                            )}
+                        </div>
+                    </div>
+                </section>
+            }
         </div>
     )
 }
 
-export default Home;
+export default Product;  
