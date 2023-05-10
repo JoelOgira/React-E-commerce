78cf2241a3312dc35cf04986596289291dd85e22
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-05-10 13:08:05 +0300

    Added functionality to the component

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,9 +1,61 @@
+import { createContext, useState, useEffect } from 'react';
 
+const CartContext = createContext({})
 
-const CartContext = () => {
-    return (
-        <div>CartContext</div>
-    )
-}
+export const CartProvider = ({ children }) => {
+    const [ cart, setCart ] = useState([]);
 
-export default CartContext;
+    const addToCart = (product, id) => {
+        const newItem = { ...product, amount: 1 }
+        // Check if item is already in the cart
+        const cartItem = cart.find(item => item?.id === id);
+        if (cartItem) {
+            const newCart = [ ...cart ].map((item) => {
+                if (item?.id === id) {
+                    return { ...item, amount: cartItem.amount + 1 };
+                } else {
+                    return item;
+                }
+            });
+            setCart(newCart);
+        } else {
+            setCart([ ...cart, newItem ]);
+        }
+    }
+
+    const removeFromCart = (id) => {
+        const newCart = cart.filter((item) => item?.id !== id);
+        setCart(newCart);
+    }
+
+    const decreaseAmount = (product, id) => {
+        const cartItem = cart.find(item => item?.id === id);
+        if (cartItem) {
+            const newCart = [ ...cart ].map((item) => {
+                if (item?.id === id) {
+                    return { ...item, amount: cartItem.amount - 1 };
+                } else {
+                    return item;
+                }
+            });
+            setCart(newCart);
+        }
+        if (cartItem.amount < 2) {
+            removeFromCart(id);
+        }
+    }
+
+        const clearCart = () => {
+            setCart([]);
+        }
+
+        return (
+            <CartContext.Provider value={{
+                cart, addToCart, removeFromCart, clearCart, decreaseAmount
+            }}>
+                {children}
+            </CartContext.Provider>
+        )
+    }
+
+    export default CartContext;
