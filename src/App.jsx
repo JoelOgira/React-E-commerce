edd87918c31a1454e8d28e24d2651e4232fb764d
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-05-10 12:59:49 +0300

    Imported components for rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './Components/E-commerce/Home';
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
+import Footer from './Components/Footer';
+import ErrorPage from './pages/ErrorPage';
+import Users from './pages/Users/Users';
 
 const App = () => {
 
   return (
-    <div className="App min-h-[100dvh] bg-slate-600 text-white">
+    <div className="App min-h-[100dvh]">
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/product/:id' element={<ProductDetails />}/>
+        <Route path='/users' element={<Users />}/>
+        <Route path='*' element={<ErrorPage />}/>
       </Routes>
+      <Sidebar />
+      <Footer />
     </div>
   )
 }
