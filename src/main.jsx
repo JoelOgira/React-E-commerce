fe856abbfcaf6da1cafea01b00457111e6be743e
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-05-10 13:03:22 +0300

    Added new Context components

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ProductProvider } from './contexts/ProductContext'
+import { SidebarProvider } from './contexts/SidebarContext'
+import { CartProvider } from './contexts/CartContext'
 import App from './App'
 import './index.css'
 
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-      <Routes>
-        <Route path='*' element={<App/>}/>
-      </Routes>
-      <ReactQueryDevtools position='bottom-left' initialIsOpen='true' />
-      </Router>
-    </QueryClientProvider>
+    <SidebarProvider>
+      <CartProvider>
+        <ProductProvider>
+          <Router>
+            <Routes>
+              <Route path='*' element={<App />} />
+            </Routes>
+          </Router>
+        </ProductProvider>
+      </CartProvider>
+    </SidebarProvider>
   </React.StrictMode>,
 )
